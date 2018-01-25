cc38606bc1bc0cffb725ef856528650df8acad1b
Karen Welch <kswelch53@gmail.com>
2018-01-25 15:22:47 -0600

    initial commit

---
+++ My_Library/urls.py
@@ -0,0 +1,24 @@
+"""My_Library URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^', include('apps.mylibrary1.urls', namespace='mylibrary1')),# login register
+    url(r'^mylibrary2/', include('apps.mylibrary2.urls', namespace='mylibrary2')),
+    url(r'^mylibrary3/', include('apps.mylibrary3.urls', namespace='mylibrary3')),
+    url(r'^admin/', admin.site.urls),
+]
